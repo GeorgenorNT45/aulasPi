5a629801e64dd9b420423a054a17e7c53c6f6826
Georgenor Neto <georgenorneto2006@gmail.com>
2024-10-15 14:05:16 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/test/java/ifrn/pi/gn/eventos/EventosApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.pi.gn.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
