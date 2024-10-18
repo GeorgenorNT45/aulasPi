bef57f984e00f55680737598e6a2c14dfafbe17f
Georgenor Neto <georgenorneto2006@gmail.com>
2024-10-17 22:35:36 -0300

    3. Unificando os parâmetros e imprimindo os valores com getters e setters

--- src/main/java/ifrn/pi/gn/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/gn/eventos/controllers/EventosController.java
@@ -2,6 +2,7 @@ package ifrn.pi.gn.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ifrn.pi.gn.eventos.models.Evento;
 
 @Controller
 public class EventosController {
@@ -12,12 +13,12 @@ public class EventosController {
 	}
 	
 	@RequestMapping("/eventos/form/enviar")
-	public String enviar(String nome, String local, String data, String horario) {
+	public String enviar(Evento evento) {
 		System.out.println("*Dados enviados*");
-		System.out.println("Nome: " + nome);
-		System.out.println("Local: " + local);
-		System.out.println("Data: " + data);
-		System.out.println("Horário: " + horario);
+		System.out.println("Nome: " + evento.getNome());
+		System.out.println("Local: " + evento.getLocal());
+		System.out.println("Data: " + evento.getData());
+		System.out.println("Horário: " + evento.getHorario());
 		return "enviar";
 	}
 	
