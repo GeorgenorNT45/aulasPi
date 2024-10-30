2590c84a96f224370b0d3978e6b7e003917ce85f
Georgenor <georgenorneto2006@gmail.com>
2024-10-30 13:54:08 -0300

    Resolvendo conflitos

---
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -0,0 +1,25 @@
+package ifrn.pi.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ifrn.pi.eventos.models.Evento;
+
+@Controller
+public class EventosController {
+
+	@RequestMapping("/eventos/form")
+	public String form() {
+		return "formEvento";
+	}
+	
+	@RequestMapping("/eventos/form/enviar")
+	public String enviar(Evento evento) {
+		System.out.println("*Dados enviados*");
+		System.out.println("Nome: " + evento.getNome());
+		System.out.println("Local: " + evento.getLocal());
+		System.out.println("Data: " + evento.getData());
+		System.out.println("Horário: " + evento.getHorario());
+		return "enviar";
+	}
+	
+}
